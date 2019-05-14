c1da6b6dc2bef103ebbdb152b8579553ddce25b4
carson <carson42g@gmail.com>
2019-05-13 21:04:44 -0400

    Scrapped rsocket, back to https. Started js integeration

--- src/js/main.js
+++ src/js/main.js
@@ -1,63 +1,29 @@
-// import {JsonSerializers} from "rsocket-core";
 
-var RSocketWebSocketClient = require("rsocket-websocket-client").default;
 
-var RSocketClient = require('rsocket-core');
-var JsonSerializers = require('rsocket-core');
 
-// import RSocketWebSocketClient from 'rsocket-websocket-client';
 
-obj.f = function f(){
-    console.log("ASDFASDF")
+let client = {
+    get: function(aUrl, aCallback,errorCallback) {
+        let anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() {
+            if (anHttpRequest.readyState === 4 && anHttpRequest.status === 200)
+                aCallback(anHttpRequest.responseText);
+            else
+                errorCallback(anHttpRequest)
+        };
+
+        anHttpRequest.open( "GET", aUrl, true );
+        anHttpRequest.send( null );
+    }
 };
 
-let rsocket = null;
-// Create an instance of a client
-const client = new RSocketClient.RSocketClient({
-    // send/receive objects instead of strings/buffers
-    serializers: JsonSerializers,
-    setup: {
-        // ms btw sending keepalive to server
-        keepAlive: 600,
-        // ms timeout if no keepalive response
-        lifetime: 1800,
-        // format of `data`
-        dataMimeType: 'application/json',
-        // format of `metadata`
-        metadataMimeType: 'application/json',
-    },
-    transport: new RSocketWebSocketClient({
-        url:"wss://72.66.54.109", port: 48626 }),
-}).connect();
 
-client.subscribe({
-    onComplete: s => {
-        rsocket = s;
-        console.log("Completed!")
-    },
-    onError: e => {
-        console.log(e)
-    }
-});
-
-console.log(typeof client);
-
-
-
-// Open the connection
-obj.ping = function(){
-    if(rsocket == null){
-        console.log("rsocket not connected yet");
-        return
-    }
-    rsocket.requestResponse({
-        data: "{ i:3 }",
-    }).subscribe({
-        onComplete: r => {
-            console.log(r)
-        },
-        onError: r => {
-            console.log(r)
-        }
+obj.ping = (callback)=>{
+    console.log("Pinging..." + typeof callback);
+    client.get(obj.url + "/hello" , (res) => {
+        console.log("Calling callbackP:" + typeof callback);
+        callback(res)
+    }, (err)=>{
+        // console.error(err)
     });
 };
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,39 @@
-private val keyfile = """
-
-""".trimIndent()
+import org.w3c.dom.HTMLButtonElement
+import kotlin.browser.document
+import kotlin.reflect.KCallable
 
 private val clientID = "b346f0ccf2"
 private val password = "javascript"
 
 external class Obj{
-    fun ping()
+    fun ping(callback :(String) -> Unit)
+    var url :String
+}
+
+fun Obj.ping(){
+    ping { println(it) }
 }
 
 external val obj :Obj
 
+val r = Release.DEV
+
+
+
 fun main() {
+    obj.url = "http://" + IP.getValue(r) + ":" + 9999 + "/api/${r.str}"
     obj.ping()
+
+//    document.getElementsByName("button")
+//        .unsafeCast<HTMLButtonElement>()
+//        .addEventListener("click",{
+//            console.log("Pinging")
+//            obj.ping()
+//        })
+    val button = document.getElementById("button") as HTMLButtonElement
+    button.addEventListener("click", {
+        println("pinging")
+        obj.ping()
+    })
 }
+
